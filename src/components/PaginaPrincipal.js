9f85912df57f6f2815d73db37c166299619a64c2
David Mejia <davidflores126@unitec.edu>
2021-02-25 16:24:54 -0600

    PrimerCommit

---
+++ src/components/PaginaPrincipal.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react'
+
+class PaginaPrincipal extends Component{
+    
+}
---
+++ src/data.js
@@ -0,0 +1,35 @@
+const data = {
+    products: [
+        {id:'1',
+        name: 'Kimetsu no Yaiba',
+        price:5.99,
+        image:"https://i.pinimg.com/originals/35/e6/89/35e6891ab791c4b3a0f08b491f9e05e1.jpg",
+    },
+    {
+        id:'2',
+        name: 'One Piece',
+        price:25.99,
+        image: " https://images-na.ssl-images-amazon.com/images/I/31A6u0Ls5NL._BO1,204,203,200_.jpg",
+
+    },
+    {
+        id:'3',
+        name: 'Dragon Ball Super',
+        price:18.99,
+       // image:
+    },
+    {
+        id: '4',
+        name: 'The Promised Neverland',
+        price:9.99,
+
+    },
+    {
+        id: '5',
+        name: 'My Hero Academia',
+        price:48.99,
+    }
+
+ ],
+};
+export default data;
