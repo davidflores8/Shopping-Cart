985c44b2309526d5fb8c904222470805453bd5aa
David Mejia <davidflores126@unitec.edu>
2021-03-10 14:44:04 -0600

    Agregando a favoritos

--- src/components/PaginaPrincipal.js
+++ src/components/PaginaPrincipal.js
@@ -1,52 +1,80 @@
 import data from "../data";
 import React, { Component } from "react";
 
+function clickMeRemove() {
+  alert("This item is going to be permanently removed from the shopping cart ");
+}
+
+function clickMeLater() {
+  alert("This item is going to be added to the saved for later");
+}
+
+function clickMe() {
+  alert("You are about to be redirected to the Checkout");
+}
+
 class PaginaPrincipal extends Component {
   constructor() {
     super();
     this.state = {
       datos: data.products,
+      favoritos: [],
       cantidad: 0,
       total: 0,
     };
   }
 
+  guardarFavoritos(index) {
+    const {favoritos: oldFavoritos } = this.state;
+    console.log("Old favoritos ",oldFavoritos)
+    oldFavoritos.push(this.state.datos[index])
+    this.setState(oldFavoritos)
+    this.quitarLibro(index)
+
+  
+
+  }
+
+  quitarLibro(index) {
+    const { datos: oldDatos } = this.state;
+    const datos = oldDatos.splice(index, 1);
+    this.setState(datos);
+    console.log("Libros despues del estado ", this.state.datos);
+  }
 
   agregarCantidad(index) {
-        const { datos: oldDatos } = this.state;
-        const datos = oldDatos.map(p => {
-          if (p.id === index && p.cantidad<10) {
-            p.cantidad += 1;
-            return p;
-          }
-          return p;
-        });
+    const { datos: oldDatos } = this.state;
+    const datos = oldDatos.map((p) => {
+      if (p.id === index && p.cantidad < 10) {
+        p.cantidad += 1;
+        return p;
+      }
+      return p;
+    });
 
-        this.setState(datos);
+    this.setState(datos);
 
-        this.setState({
-          total: Math.round((this.state.total + datos[index].price))
-        })
+    this.setState({
+      total: Math.round(this.state.total + datos[index].price),
+    });
   }
 
   quitarCantidad(index) {
-        const { datos: oldDatos } = this.state;
-        const datos = oldDatos.map(p => {
-          if (p.id === index && p.cantidad>0) {
-            p.cantidad -= 1;
-            return p;
-          }
-          return p;
-        });
+    const { datos: oldDatos } = this.state;
+    const datos = oldDatos.map((p) => {
+      if (p.id === index && p.cantidad > 0) {
+        p.cantidad -= 1;
+        return p;
+      }
+      return p;
+    });
 
-        this.setState(datos);
+    this.setState(datos);
 
-        this.setState({
-          total: Math.round((this.state.total - datos[index].price))
-        })
+    this.setState({
+      total: Math.round(this.state.total - datos[index].price),
+    });
   }
-
-
 
   render() {
     return (
@@ -54,9 +82,22 @@ class PaginaPrincipal extends Component {
         <section>
           <div className="row">
             <div className="col-lg-8">
-              <div className="card  mb-3">
+              <div className="card wish-list mb-3">
                 <div className="card-body">
-                  <h5 className="mb-4">Carrito</h5>
+                  <h5 className="text-center mb-5">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      fill="currentColor"
+                      className="mx-3 bi bi-basket"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
+                    </svg>
+                    My Shopping Cart{" "}
+                  </h5>
+
                   {this.state.datos.map((dato, index) => {
                     return (
                       <div key={dato.id} className="row mb-4">
@@ -75,13 +116,14 @@ class PaginaPrincipal extends Component {
                               <div>
                                 <h5 className="mb-5">{dato.name}</h5>
                                 <p className="mb-3 text-muted text-uppercase small">
-                                  Camisa - azul
+                                  Format - Hardcover
                                 </p>
                                 <p className="mb-3 text-muted text-uppercase small">
-                                  Color: azul
+                                  Language: English
                                 </p>
-                                <p className="mb-3 text-muted text-uppercase small">
-                                  Tamaño: M
+                                <p className="fst-italic">
+                                  Choose Expedited Shipping at checkout for
+                                  guaranteed delivery by Wednesday, March 10
                                 </p>
                               </div>
                               <div>
@@ -115,7 +157,7 @@ class PaginaPrincipal extends Component {
                                   id="passwordHelpBlock"
                                   className="form-text text-muted text-center"
                                 >
-                                  Cantidad
+                                  Quantity
                                 </small>
                               </div>
                             </div>
@@ -125,60 +167,75 @@ class PaginaPrincipal extends Component {
                                   href="#!"
                                   type="button"
                                   className="card-link-secondary small text-uppercase mr-3"
+                                  onClick={() => this.quitarLibro(index)}
                                 >
-                                  <i className="fas fa-trash-alt mr-1"></i>{" "}
-                                  Quitar item
+                                  Remove item
                                 </a>
                                 <a
                                   href="#!"
                                   type="button"
                                   className="card-link-secondary small text-uppercase"
+                                  onClick={()=>this.guardarFavoritos(index)}
                                 >
-                                  <i className="fas fa-heart mr-1"></i> Añadir a
-                                  favoritos{" "}
+                                  Add to Saved for later
                                 </a>
                               </div>
                               <p className="mb-0">
                                 <span>
-                                  <strong>{dato.price}</strong>
+                                  <strong>${dato.price}</strong>
                                 </span>
                               </p>
                             </div>
                           </div>
                         </div>
+                        <hr />
                       </div>
-                       
                     );
                   })}
                 </div>
               </div>
-              
+
               <div className="card mb-3">
                 <div className="card-body">
-                  <h5 className="mb-4">Tiempo de entrega</h5>
-                     
-                  <p className="mb-0"> Martes o Miércoles.</p>
+                  <h5 className="mb-4">SAVED FOR LATER Items</h5>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-heart"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                  </svg>
+
+                  <p className="mb-0"> No items</p>
                 </div>
               </div>
             </div>
             <div className="col-lg-4">
               <div className="card mb-3">
                 <div className="card-body">
-                  <h5 className="mb-3">El total es</h5>
+                  <h5 className="text-center"> Order Summary</h5>
+                  <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                    Estimated Taxes:
+                    <span>$0.00</span>
+                  </li>
+
                   <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                      Entrega
-                      <span>Gratis</span>
+                      Estimated Shipping:
+                      <span>$0.01</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                       <div>
-                        <strong>El total es</strong>
+                        <strong>Order Total:</strong>
                         <strong>
-                          <p className="mb-0">(incluyendo descuento)</p>
+                          <p className="mb-0"></p>
                         </strong>
                       </div>
                       <span>
-                        <strong>$ {this.state.total}</strong>
+                        <strong>$ {this.state.total}.00</strong>
                       </span>
                     </li>
                   </ul>
@@ -186,8 +243,20 @@ class PaginaPrincipal extends Component {
                   <button
                     type="button"
                     className="btn btn-primary btn-block waves-effect waves-light"
+                    onClick={clickMe}
                   >
-                    Ir a total
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-cash-stack"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
+                      <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z" />
+                    </svg>
+                    CHECKOUT
                   </button>
                 </div>
               </div>
